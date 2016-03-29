a4ad60134c8ae70107d07ffc117a2010eb6fac9e
IceMao <18845294217@qq.com>
2016-03-29 13:14:45 +0800

    ice-mao
    临时 一些页面

---
+++ iceWebPage/ajax/ajax.js
@@ -0,0 +1,25 @@
+﻿//封装Ajax.js
+function ajax(url,fnSuccess,fnFail) {
+    //1.创建
+    var oAjax = null;
+    if (window.XMLHttpRequest) {
+        oAjax = new XMLHttpRequest();
+    } else {
+        oAjax = new ActiveXObject("Microsoft.XMLHttp");
+    }
+    //2.连接
+    oAjax.open('GET',url,true);
+    //3.发送
+    oAjax.send();
+    //4.接收返回
+    oAjax.onreadystatechange = function () {
+        if (oAjax.readyState == 4) {
+            if (oAjax.status == 200) {
+                fnSuccess(oAjax.responseText());
+            } else {
+                alert("文件读取失败");
+            }
+        }
+    }
+    
+};
---
+++ iceWebPage/scripts/index.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $(window).load().scroll(function () {
+        var sTop = $(document).scrollTop();
+        if (sTop >= 200) {
+            $("#toTop").fadeIn(300);
+        } else {
+            $("#toTop").fadeOut(300);
+        }
+        if (sTop >= 272) {
+            //$(".index-nav").css({ height: "0px" });
+            $(".index-nav").addClass("fix");
+        } else {
+            $(".index-nav").removeClass("fix");
+        }
+    });
+    $(".header").hover(function () {
+        $(this).find("h1").css("color", "#fff");
+        $("small").stop(true,false).animate({
+            opacity: '1',
+            right:"340px"
+        },800);
+    }, function () {
+        $(this).find("h1").css("color", "#efefef");
+        $("small").stop(true,false).animate({
+            opacity: '0',
+            right:"220px"
+        },800);
+    });
+    
+    $(".menu li").click(function () {
+        $(this).siblings().removeClass("nav-current");
+        $(this).addClass("nav-current");
+    });
+    $("#toTop").click(function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+                scrollTop: 0
+            }, 500);
+        return false;
+    });
+});
